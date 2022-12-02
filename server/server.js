7b3c5f64d66648aa418ffada7870f9ae1973286b
Devin <Devin2152c@gmail.com>
2022-12-01 22:43:50 -0600

    Initial Commit with NodeJs Express Server running

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const client = require('./elasticsearch/client');
+const app = express();
+
+const port = 3001;
+
+app.listen(port, () => console.log('Server listening at http://localhost:${port}'));
